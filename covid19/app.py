1e00af8af72ab2761bbb97e8179837ee6189767d
acentos <acentos@localhost>
2021-05-19 10:59:19 +0000

    add: Dockerfile

--- covid19/app.py
+++ covid19/app.py
@@ -3,4 +3,4 @@ from covid19_stat import app, create_app
 
 if __name__ == '__main__':
     create_app()
-    app.run(host='10.8.0.1', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
